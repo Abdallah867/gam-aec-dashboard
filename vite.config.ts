e176aa8de6c8f3d30ea45dc4f57f224c1812c651
Abdallah <abdoallahusma5704@gmail.com>
2026-04-18 14:33:31 +0100

    netlify issue solving

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import neon from './neon-vite-plugin.ts'
+import netlify from '@netlify/vite-plugin-tanstack-start' // ← add this
 
 const config = defineConfig({
   plugins: [
+    netlify(),
     devtools(),
     neon,
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
